81cc67d044ad7cf75601feb994dd021fd4c6d207
Emeka <emeka@dyn-147-4.public.tut.fi>
2017-09-11 15:46:42 +0300

    Added Mongo Cloud Cluster

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const env = process.env;
 
 export default{
-    mongodbUri: 'mongodb://localhost:27017/user_app',
+    mongodbUri: 'mongodb://emmanuel:Mongodb11.@userapp-shard-00-00-epqyj.mongodb.net:27017,userapp-shard-00-01-epqyj.mongodb.net:27017,userapp-shard-00-02-epqyj.mongodb.net:27017/test?ssl=true&replicaSet=userApp-shard-0&authSource=admin',
     port:env.PORT || 8080,
     host: env.HOST || '0.0.0.0',
     get serverUrl(){
